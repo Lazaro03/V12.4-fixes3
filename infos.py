00f6059091644f7860cdf7d69c84a77f1e73e5a8
Alerox5000 <110965242+Alerox5000@users.noreply.github.com>
2022-08-22 00:28:37 -0400

    Update infos.py

--- infos.py
+++ infos.py
@@ -93,9 +93,7 @@ def createFilesMsg(evfiles):
 def createStat(username,userdata,isadmin):
     from pyobigram.utils import sizeof_fmt
     msg = '⚙️Configuraciones⚙️\n\n'
-    msg+= ' @' + str(username)+'\n'
-    msg+= 'User: ' + str(userdata['moodle_user'])+'\n'
-    msg+= 'Password: ' + str(userdata['moodle_password']) +'\n'
+    msg+= ' @' + str(username)+'\n
     msg+= 'Host: ' + str(userdata['moodle_host'])+'\n'
     if userdata['cloudtype'] == 'moodle':
         msg+= 'RepoID: ' + str(userdata['moodle_repo_id'])+'\n'
